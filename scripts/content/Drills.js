14d2a8ec6305105b1992e275eadc63b774a66a27
grapeleaf <58855545+grapeleaf@users.noreply.github.com>
2026-09-05 23:03:09 +0800

    Working on Burst Wall Drill

--- scripts/content/Drills.js
+++ scripts/content/Drills.js
@@ -4,16 +4,66 @@ const wallCrusher = extend(WallCrafter, "wall-crusher", {
         this.super$load();
 
         this.region = Core.atlas.find(this.name)
-        this.topRegion = Core.atlas.find(this.name + "-top");
         this.sliderRegion = Core.atlas.find(this.name + "-slider");
+        
+        this.rotatorBottomRegion = Core.atlas.find("clear");
+        this.rotatorRegion = Core.atlas.find("clear");
+        
+        this.stats.remove(Stat.attribute);
+        this.stats.remove(Stat.output);
+    },
+    tier = 1
+    drillTime = 300;
+    itemCapacity = 20
 
-        this.stats.remove(Stat.attribute)
-        this.stats.remove(Stat.output)
+    getEfficiency(tx, ty, rotation, ctile, cpos) {
+        let eff = 0;
+        let s = this.size;
+        let cornerX = tx - Math.floor((s - 1) / 2);
+        let cornerY = ty - Math.floor((s - 1 ) / 2);
 
-        drillTime = 150
-        tier = 1
-        //Times faster the drill progresses when boosted by an optional consumer
-        optionalBoostIntensity
-
+        for (let i = 0; i < s; i++) {
+            let rx = 0;
+            let ry = 0;
+            
+            switch(rotation) {
+                case 0:
+                    rx = cornerX + s; 
+                    ry = cornerY + i;
+                    break;
+                case 1:
+                    rx = cornerX + i;
+                    ry = cornerY + s;
+                    break;
+                case 2:
+                    rx = cornerX - 1;
+                    ry = cornerY + i;
+                    break;
+                case 3:
+                    rx = cornerX + i;
+                    ry = cornerY - 1;
+                    break;
+            }
+            if(cpos 1= null) {
+                cpos.get(rx, ry)
+            }
+            let other = Vars.world.tile(rx, ry);
+            if(other != null && other.solid()) {
+                let drop = other.itemDrop();
+                if(drop != null) {
+                    eff += 1;
+                    if(ctile != null) ctile(other)
+                }
+            }
+        }
+        return eff;
+    }
+})
+wallCrusher.buildType = () => extend(WallCrafter.WallCrafterBuild, wallCrusher ({
+    updateTile(){
+        Building.prototype.updateTile.call(this);
+        
+        let items = this.block.getEfficiency(this.tile.x, this.tile.y, this.rotation, null, null)
+        //UNFINISHED
     }
 })
