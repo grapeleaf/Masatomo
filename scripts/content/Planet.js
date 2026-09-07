d958d5990ba5c61c2278e7cb9b618a8fafa17bd9
GrapeLeaf <minecartbow12@gmail.com>
2026-09-07 08:08:18 +0800

    Replaced pulverizer with new custom burst wall crafter

---
+++ scripts/content/Planet.js
@@ -0,0 +1,7 @@
+const Planet = Packages.mindustry.type.Planet;
+const Planets = Packages.mindustry.content.Planets;
+const HexSkyMesh = Packages.mindustry.graphics.g3d.HexSkyMesh;
+const MultiMesh = Packages.mindustry.graphics.g3d.MultiMesh;
+const SunMesh = Packages.mindustry.graphics.g3d.SunMesh;
+const HexMesh = Packages.mindustry.graphics.g3d.HexMesh;
+const NoiseMesh = Packages.mindustry.graphics.g3d.NoiseMesh;
